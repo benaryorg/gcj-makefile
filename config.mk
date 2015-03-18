71fef44dd38a9994559d84ceb089995d578cb690
benaryorg <binary@benary.org>
2015-03-18 15:01:38 +0100

    bugfix

--- config.mk
+++ config.mk
@@ -6,6 +6,6 @@ MAINCLASS:=main.Main
 JFLAGS:=--classpath=$(CLASSPATH)
 
 SRCS=$(shell find $(CLASSPATH) -iname '*.java')
-OBJS=$(addprefix build/,$(addsuffix .class,$(basename $(SRCS:$(CLASSPATH)%=%))))
-CLSS=$(addprefix build/,$(addsuffix .o,$(basename $(SRCS:$(CLASSPATH)%=%))))
+CLSS=$(addprefix build/,$(addsuffix .class,$(basename $(SRCS:$(CLASSPATH)%=%))))
+OBJS=$(addprefix build/,$(addsuffix .o,$(basename $(SRCS:$(CLASSPATH)%=%))))
 
