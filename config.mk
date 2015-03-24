2c78dcec4c398b66fdb247258fa9dd1bab1118bc
benaryorg <binary@benary.org>
2015-03-24 19:22:56 +0100

    added jar support

--- config.mk
+++ config.mk
@@ -1,11 +1,12 @@
 GCJ:=gcj
 
 CLASSPATH:=src/
+RESPATH:=res/
 MAINCLASS:=main.Main
-
-JFLAGS:=--classpath=$(CLASSPATH)
 
 SRCS=$(shell find $(CLASSPATH) -iname '*.java')
 CLSS=$(addprefix build/,$(addsuffix .class,$(basename $(SRCS:$(CLASSPATH)%=%))))
 OBJS=$(addprefix build/,$(addsuffix .o,$(basename $(SRCS:$(CLASSPATH)%=%))))
+JARS=$(shell find $(RESPATH) -iname '*.jar')
 
+JFLAGS:=-I$(CLASSPATH) $(addprefix -I,$(shell find $(RESPATH) -iname '*.jar'))
